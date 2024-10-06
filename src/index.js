27a0e59577cb324410093750e3152acb58e3d1b3
Your Name <usamax089@gmail.com>
2024-10-06 11:39:29 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const taskForm = document.getElementById("create-task-form");
+ 
+  taskForm.addEventListener("submit", function (event) {
+   event.preventDefault();
+ 
+   const color = document.getElementById("select-colour").value;
+   const taskDescription = document.getElementById("new-task-description").value;
+   if (taskDescription.trim() !== ""){
+     addTaskToList(taskDescription, color);
+     taskForm.reset();
+   }
+  });
+ });
+ 
+ function addTaskToList(task, color) {
+   const taskList = document.getElementById("tasks");
+   const newTask = document.createElement("li");
+   newTask.textContent = task;
+   newTask.style.color = color;
+ 
+   const deleteButton = document.createElement("button")
+   deleteButton.textContent = "Delete";
+   deleteButton.addEventListener("click", () => {
+     newTask.remove();
+   });
+ 
+   newTask.appendChild(deleteButton);
+   taskList.appendChild(newTask,);
+ }
